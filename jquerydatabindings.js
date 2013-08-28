951e3b9f4921c2a496ace9815e73f52f8e052eb2
Andrija Petrovic <andrija.hers@gmail.com>
2013-08-28 09:38:41 +0200

    Data binding is now flat only - down to first level fields

--- jquerydatabindings.js
+++ jquerydatabindings.js
@@ -1,8 +1,11 @@
-function jqueryBindToScalar(collection,path,jqueryobj,selector,behavior){
+function jqueryBindToScalar(collection,scalarname,jqueryobj,selector,behavior){
+  if(typeof scalarname !== 'string'){
+    throw "Scalarname "+scalarname+" has to be a string";
+  }
   if(!(behavior&&(behavior.activator||behavior.deactivator||behavior.setter))){
     return;
   }
   var jqo = selector ? (function(jqo,sel){return function(){return jqo.find(sel);}})(jqueryobj,selector) : (function(jqo){return function(){return jqo};})(jqueryobj);
-  return listenToCollectionField(jqo,collection,path[0],behavior);
+  return listenToCollectionField(jqo,collection,scalarname,behavior);
 };
 
